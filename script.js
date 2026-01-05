a8742627f38d66f25608464045f7f353633e78cf
CVFIncorporadora <cvf@cvfincorporadora.com.br>
2026-01-05 15:12:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let lotes = [];
+
+fetch("lotes.json")
+    .then(res => res.json())
+    .then(data => {
+        lotes = data;
+        montarTabela();
+    });
+
+function montarTabela() {
+    const tbody = document.querySelector("#tabela tbody");
+    tbody.innerHTML = "";
+
+    lotes.forEach((lote, index) => {
+        const tr = document.createElement("tr");
+        tr.className = lote.Vendido === "TRUE" ? "vendido" : "livre";
+
+        tr.innerHTML = `
+            <td>${lote.ID}</td>
+            <td>${lote.Nome}</td>
+            <td>${lote.Area}</td>
+
+            <td>
+                <input 
+                    type="number" 
+                    value="${lote.Valor}"
+                    style="width:120px"
+                    onchange="alterarValor(${index}, this.value)"
+                >
+            </td>
+
+            <td>
+                <select onchange="alterarVendido(${index}, this.value)">
+                    <option value="FALSE" ${lote.Vendido === "FALSE" ? "selected" : ""}>Disponível</option>
+                    <option value="TRUE" ${lote.Vendido === "TRUE" ? "selected" : ""}>Vendido</option>
+                </select>
+            </td>
+        `;
+        tbody.appendChild(tr);
+    });
+}
+
+function alterarValor(index, novoValor) {
+    lotes[index].Valor = Number(novoValor);
+}
+
+function alterarVendido(index, valor) {
+    lotes[index].Vendido = valor;
+    montarTabela();
+}
+
+function salvar() {
+    const json = JSON.stringify(lotes, null, 2);
+    const blob = new Blob([json], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "lotes_atualizado.json";
+    a.click();
+
+    URL.revokeObjectURL(url);
+}
+
