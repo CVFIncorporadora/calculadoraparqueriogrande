4f56e72b398819c8656ba8b85950acbf4c9b2dcd
Felipe Molz <felipeborbamolz@gmail.com>
2026-01-09 16:41:54 -0300

    corrigido link de referência do render

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const API_BASE = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
   ? "http://localhost:3000"
-  : "https://backend-parque-riogrande.onrender.com";
+  : "https://calculadoraparqueriogrande.onrender.com";
 
 let lotes = [];
 let filtroEmpreendimentoValue = "";
